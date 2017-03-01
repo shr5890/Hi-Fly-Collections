1c12e9c923bbce2488007cf213aba73883b7e983
Hifzur Rahman <hifzurrahman05081990@gmail.com>
2017-03-01 13:31:21 +0530

    Added class for Xuggler

--- Hi Fly Collections/src/textencryptor/TextEncrytorMain.java
+++ Hi Fly Collections/src/textencryptor/TextEncrytorMain.java
@@ -3,7 +3,7 @@ package textencryptor;
 public class TextEncrytorMain {
 	public static void main(String[] args){
 		TextReader TR = new TextReader();
-		TR.runEncryptor("hi helllo ho u doin??\nwhats up bro?\nHow is ur family?\nTC see ya:)\nPoda poda poda porambokku");
+		TR.runEncryptor("hi helllo ho u doin??\nwhats up bro?\nHow is ur family?\nTC see ya:)\nhell yeah :) :( :> - hey hey jey jey");
 		new TextWriter().decryptMessage(TR.sbEncryptedMessage.toString(), TR.sbMessageKey.toString());
 	}
 }
