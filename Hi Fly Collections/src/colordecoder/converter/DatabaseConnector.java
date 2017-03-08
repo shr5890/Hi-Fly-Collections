4bcc13b74ffa3c54d62664c2faaa5ab8a07d6af8
Hifzur Rahman <hifzurrahman05081990@gmail.com>
2017-03-08 19:38:21 +0530

    DB Connector

---
+++ Hi Fly Collections/src/colordecoder/converter/DatabaseConnector.java
@@ -0,0 +1,44 @@
+package colordecoder.converter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+ 
+public class DatabaseConnector {
+ 
+    public static void main(String[] args) throws ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306/contactdb";
+        String user = "root";
+        String password = "secret";
+ 
+        String filePath = "D:/Photos/Tom.png";
+ 
+        try {
+        	Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            
+            String sql = "INSERT INTO person (first_name, last_name, photo) values (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, "Tom");
+            statement.setString(2, "Eagar");
+            InputStream inputStream = new FileInputStream(new File(filePath));
+ 
+            statement.setBlob(3, inputStream);
+ 
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                System.out.println("A contact was inserted with photo image.");
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
